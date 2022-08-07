4c891f2f78dac96fb6a8089ddd75f4726efbbccf
Vadim1707 <vadimsedov1707@gmail.com>
2022-08-07 12:54:20 +0300

    Modified engine for DB access

--- config.py
+++ config.py
@@ -1,7 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import database_exists, create_database
+from local_settings import postgresql as settings
 
 DATABASE_URL = ""
 
-engine = create_engine("postgresql+psycopg2://postgres:__enter_password__@localhost:5432/European")
+def get_engine(user, passwd, host, port, db):
+    url = f"postgresql://{user}:{passwd}@{host}:{port}/{db}"
+    if not database_exists(url):
+        create_database(url)
+    engine = create_engine(url, pool_size=50, echo=False)
+    return engine
+
+engine = get_engine(settings['pguser'],
+        settings['pgpassword'],
+        settings['pghost'],
+        settings['pgport'],
+        settings['pgdb'])
+
+print(engine.url)
